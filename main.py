9b5d8f678f4b190b546877e0289952718eb336d3
yuvraj333 <yuvrajkumarmahato@gmail.com>
2026-08-24 18:18:32 +0545

    final: push

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     parser.add_argument("--val_split", type=float, default=0.15, help="Validation ratio")
     args = parser.parse_args()
 
-    # Step 1: Load and preprocess data
+    #  Load and preprocess data
     X_train, X_test, y_train, y_test, class_names = load_and_preprocess_data(
         raw_data_path=args.data_path,
         sample_per_class=args.samples_per_class
@@ -22,14 +22,14 @@ def main():
     input_shape = (X_train.shape[1], 1)
     num_classes = len(class_names)
 
-    # Step 2: Model list
+    # Model list
     pipeline = [
         build_1d_cnn(input_shape, num_classes),
         build_rnn_lstm(input_shape, num_classes),
         build_hybrid_cnn_lstm(input_shape, num_classes)
     ]
 
-    # Step 3: Run pipeline
+    # Run pipeline
     for model in pipeline:
         trained_model, _ = train_model(
             model=model,
